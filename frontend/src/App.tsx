eaf8997dea8878c15738074f5d517bc35b45f86d
Yogeeta Y <yadhvi.ch@gmail.com>
2025-11-29 12:13:18 +0530

    main

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,36 @@
-import { Button } from "@/components/ui/button"
-import { SignedOut, SignedIn, SignInButton, UserButton } from '@clerk/clerk-react'
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layout/MainLayout";
+import ChatPage from "./pages/chat/ChatPage";
+import AlbumPage from "./pages/album/AlbumPage";
+import AdminPage from "./pages/admin/AdminPage";
 
+import { Toaster } from "react-hot-toast";
+import NotFoundPage from "./pages/404/NotFoundPage";
 
-const App = () => {
-  return (
-    <>
-      <header>
-        <SignedOut>
-          <SignInButton>
-            <Button variant={"destructive"}> Sign In </Button>
-          </SignInButton>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </header>
-    </>
-  )
+function App() {
+	return (
+		<>
+			<Routes>
+				<Route
+					path='/sso-callback'
+					element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"} />}
+				/>
+				<Route path='/auth-callback' element={<AuthCallbackPage />} />
+				<Route path='/admin' element={<AdminPage />} />
+
+				<Route element={<MainLayout />}>
+					<Route path='/' element={<HomePage />} />
+					<Route path='/chat' element={<ChatPage />} />
+					<Route path='/albums/:albumId' element={<AlbumPage />} />
+					<Route path='*' element={<NotFoundPage />} />
+				</Route>
+			</Routes>
+			<Toaster />
+		</>
+	);
 }
 
-export default App
+export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { BrowserRouter } from "react-router-dom";
+import AuthProvider from "./providers/AuthProvider.tsx";
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -15,7 +17,11 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+     <AuthProvider>
+				<BrowserRouter>
         <App />
+        </BrowserRouter>
+			</AuthProvider>
       </ClerkProvider>
   </StrictMode>,
 )
